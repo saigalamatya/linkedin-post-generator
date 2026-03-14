0376724425c8159933eaabf8d6b998e3beb5f63e
saigalamatya <saigal.amatya@gmail.com>
2026-03-14 20:57:28 +0000

    preprocess.py changes

--- llm_helper.py
+++ llm_helper.py
@@ -4,7 +4,10 @@ import os
 
 load_dotenv()
 
-llm = ChatGroq(groq_api_key=os.getenv("GROQ_API_KEY"), model_name="meta-llama/llama-4-scout-17b-16e-instruct")
+llm = ChatGroq(
+    groq_api_key=os.getenv("GROQ_API_KEY"), 
+    model_name="meta-llama/llama-4-scout-17b-16e-instruct"
+)
 
 if __name__ == "__main__":
     response = llm.invoke("What are the main ingredients in Mo: Mo:")
--- preprocess.py
+++ preprocess.py
@@ -1,27 +1,44 @@
 import json
+from llm_helper import llm
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.exceptions import OutputParserException
 
 def process_posts(raw_file_path, processed_file_path="data/processed_posts.json"):
     enriched_posts = []
     with open(raw_file_path, encoding='utf-8') as file:
         posts = json.load(file)
-        print(posts)
 
         for post in posts:
-            metadata = extract_metadata(post['Content'])
+            metadata = extract_metadata(post['text'])
             post_with_metadata = post | metadata
             enriched_posts.append(post_with_metadata)
 
         for epost in enriched_posts:
             print(epost)
 
-    pass 
-
 def extract_metadata(post):
-    return {
-        'line_count': 10,
-        'language': 'English',
-        'tags': ['LLM', 'AI', 'Gen AI']
-    }
+    template = '''
+    You are given a LinkedIn post. You need to extract number of lines, language of the post and tags.
+    1. Return a valid JSON. No preamble. 
+    2. JSON object should have exactly three keys: line_count, language and tags. 
+    3. tags is an array of text tags. Extract maximum two tags.
+    4. Language should be English or Hinglish (Hinglish means hindi + english)
+    
+    Here is the actual post on which you need to perform this task:  
+    {post}
+    '''
+
+    pt = PromptTemplate.from_template(template)
+    chain = pt | llm
+    response = chain.invoke(input={'post': post})
+
+    try:
+        json_parser = JsonOutputParser()
+        res = json_parser.parse(response.content)
+    except OutputParserException:
+        raise OutputParserException("Context too big. Unable to parse jobs.")
+    return res
 
 if __name__ == "__main__":
-    process_posts("data/data.json", "data/processed_posts.json")
+    process_posts("data/raw_posts.json", "data/processed_posts.json")
