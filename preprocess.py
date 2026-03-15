16dcb9e8d5676e202542a9fd8d1713798e993166
saigalamatya <saigal.amatya@gmail.com>
2026-03-15 12:20:08 +0000

    unified tags process

--- preprocess.py
+++ preprocess.py
@@ -4,18 +4,65 @@ from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import JsonOutputParser
 from langchain_core.exceptions import OutputParserException
 
+
+def sanitize_text(text):
+    if not isinstance(text, str):
+        return text
+    return text.encode("utf-16", "surrogatepass").decode("utf-16", "ignore")
+
 def process_posts(raw_file_path, processed_file_path="data/processed_posts.json"):
     enriched_posts = []
     with open(raw_file_path, encoding='utf-8') as file:
         posts = json.load(file)
 
         for post in posts:
-            metadata = extract_metadata(post['text'])
-            post_with_metadata = post | metadata
+            safe_text = sanitize_text(post['text'])
+            metadata = extract_metadata(safe_text)
+            post_with_metadata = post | {'text': safe_text} | metadata
             enriched_posts.append(post_with_metadata)
 
-        for epost in enriched_posts:
-            print(epost)
+    unified_tags = get_unified_tags(enriched_posts)
+
+    for post in enriched_posts:
+        current_tags = post['tags']
+        new_tags = {unified_tags[tag] for tag in current_tags}
+        post['tags'] = list(new_tags)
+
+    with open(processed_file_path, encoding='utf-8', mode='w') as outfile:
+        json.dump(enriched_posts, outfile, indent=4)    
+
+def get_unified_tags(posts_with_metadata):
+    unique_tags = set()
+    for post in posts_with_metadata:
+        unique_tags.update(post['tags'])
+
+    unique_tags_list = ', '.join(unique_tags)
+
+    template = '''I will give you a list of tags. You need to unify tags with the following requirements,
+    1. Tags are unified and merged to create a shorter list. 
+    Example 1: "Jobseekers", "Job Hunting" can be all merged into a single tag "Job Search". 
+    Example 2: "Motivation", "Inspiration", "Drive" can be mapped to "Motivation"
+    Example 3: "Personal Growth", "Personal Development", "Self Improvement" can be mapped to "Self Improvement"
+    Example 4: "Scam Alert", "Job Scam" etc. can be mapped to "Scams"
+    2. Each tag should be follow title case convention. example: "Motivation", "Job Search"
+    3. Output should be a JSON object, No preamble
+    3. Output should have mapping of original tag and the unified tag. 
+    For example: {{"Jobseekers": "Job Search",  "Job Hunting": "Job Search", "Motivation": "Motivation}}
+    
+    Here is the list of tags: 
+    {tags}
+    '''
+
+    pt = PromptTemplate.from_template(template)
+    chain = pt | llm
+    response = chain.invoke(input={"tags": sanitize_text(unique_tags_list)})
+
+    try:
+        json_parser = JsonOutputParser()
+        res = json_parser.parse(response.content)
+    except OutputParserException:
+        raise OutputParserException("Context too big. Unable to parse jobs.")
+    return res
 
 def extract_metadata(post):
     template = '''
@@ -31,7 +78,7 @@ def extract_metadata(post):
 
     pt = PromptTemplate.from_template(template)
     chain = pt | llm
-    response = chain.invoke(input={'post': post})
+    response = chain.invoke(input={"post": sanitize_text(post)})
 
     try:
         json_parser = JsonOutputParser()
